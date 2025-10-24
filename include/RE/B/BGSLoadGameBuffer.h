acd77a2397cd293db0e6bd03dcbe825993247cbf
Qudix <17361645+Qudix@users.noreply.github.com>
2025-10-24 18:34:28 -0500

    feat: `BGSLoadGameBuffer` & `BGSSaveGameBuffer`

---
+++ include/RE/B/BGSLoadGameBuffer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace RE
+{
+	class __declspec(novtable) BGSLoadGameBuffer
+	{
+	public:
+		static constexpr auto RTTI{ RTTI::BGSLoadGameBuffer };
+		static constexpr auto VTABLE{ VTABLE::BGSLoadGameBuffer };
+
+		virtual ~BGSLoadGameBuffer();  // 00
+
+		// add
+		virtual std::uint8_t GetVersion(void);  // 01
+
+		void LoadDataEndian(void* a_data, std::uint32_t a_offset, std::uint32_t a_size)
+		{
+			using func_t = decltype(&BGSLoadGameBuffer::LoadDataEndian);
+			static REL::Relocation<func_t> func{ REL::ID(1451631) };
+			return func(this, a_data, a_offset, a_size);
+		}
+
+		// members
+		void*         buffer;          // 008
+		std::uint64_t unk10;           // 010
+		std::uint32_t unk18;           // 018
+		std::uint32_t unk1C;           // 01C
+		std::uint32_t unk20;           // 020
+		std::uint32_t bufferPosition;  // 024
+	};
+	static_assert(sizeof(BGSLoadGameBuffer) == 0x28);
+}
