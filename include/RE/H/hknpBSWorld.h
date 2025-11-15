c4a6e579a8eb43c5ae435dbcde92245ff19bfc54
Mathias Dejerud Rådstam <68670981+FlenarnDev@users.noreply.github.com>
2025-11-15 18:44:27 +0100

    po3 cherrypicking (#54)
    * feat: Cherrypicking from PO3 fork.
    
    * maintenance
    
    ---------
    
    Co-authored-by: Flenarn <flenarn@hotmail.com>
    Co-authored-by: FlenarnDev <FlenarnDev@users.noreply.github.com>

---
+++ include/RE/H/hknpBSWorld.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "RE/B/BSSpinLock.h"
+#include "RE/B/BSTHashMap.h"
+#include "RE/H/hkMap.h"
+#include "RE/H/hknpWorld.h"
+
+namespace RE
+{
+	class __declspec(novtable) hknpBSWorld :
+		public hknpWorld
+	{
+	public:
+		static constexpr auto RTTI{ RTTI::hknpBSWorld };
+		static constexpr auto VTABLE{ VTABLE::hknpBSWorld };
+
+		virtual ~hknpBSWorld() override;  // 00
+
+		// members
+		void*                     userData;                     // 6D0
+		BSReadWriteLock           worldLock;                    // 6D8
+		BSTSet<hknpBodyId>        activateBodyPostCollideSet;   // 6E0
+		BSSpinLock                activateBodyPostCollideLock;  // 710
+		hkMultiMap<std::uint32_t> constrainedBodyFromBody;      // 718
+	};
+	static_assert(sizeof(hknpBSWorld) == 0x730);
+}
