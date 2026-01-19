7c36cdd6052850cbc490f4db601cf9ba161b1dc1
Qudix <17361645+Qudix@users.noreply.github.com>
2026-01-19 14:58:00 -0600

    feat: add rotating log option

--- include/F4SE/API.h
+++ include/F4SE/API.h
@@ -29,6 +29,7 @@ namespace F4SE
 #endif
 		const char* logName{ nullptr };
 		const char* logPattern{ nullptr };
+		std::size_t logRotate{ 0 };
 		bool        trampoline{ false };
 		std::size_t trampolineSize{ 0 };
 		bool        hook{ true };
