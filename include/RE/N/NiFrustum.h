8a45bab400efa87a814f5669305f35e56f064f66
Flenarn <flenarn@hotmail.com>
2026-08-05 14:17:39 +0200

    fix: Corrections from review.

--- include/RE/N/NiFrustum.h
+++ include/RE/N/NiFrustum.h
@@ -5,13 +5,13 @@ namespace RE
 	class NiFrustum
 	{
 	public:
-		float fLeft;    // 00
-		float fRight;   // 04
-		float fTop;     // 08
-		float fBottom;  // 0C
-		float fNear;    // 10
-		float fFar;     // 14
-		bool  ortho;    // 18
+		float left;    // 00
+		float right;   // 04
+		float top;     // 08
+		float bottom;  // 0C
+		float near;    // 10
+		float far;     // 14
+		bool  ortho;   // 18
 	};
 	static_assert(sizeof(NiFrustum) == 0x1C);
 }
